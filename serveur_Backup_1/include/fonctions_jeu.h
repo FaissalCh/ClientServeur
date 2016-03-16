e303f6cf58e07e7a6c51eccaeed3183665c6f2a4
faissalCh <fsstach@hotmail.com>
2016-03-16 17:20:44 +0100

    Message de validation

---
+++ serveur_Backup_1/include/fonctions_jeu.h
@@ -0,0 +1,6 @@
+#ifndef Fonction_Jeu_H
+#define Fonction_Jeu_H
+
+char *getBilanSession(Session *s);
+
+#endif 
---
+++ serveur_Backup_1/src/fonctions_jeu.c
@@ -0,0 +1,28 @@
+#include <tools.h>
+#include <types_jeu.h>
+#include <fonctions_jeu.h>
+
+#define T_ENI 2048 // Faire petit calcul pour borner la taille
+
+
+// Mettre dans session.(c|h)
+char *getBilanSession(Session *s) {
+  // Prendre mutex session
+  int nbTour = s->nbTour;
+  ListeJoueurs *l = s->liste;
+  char res[T_ENI];
+  char tmp[T_ENI];
+  Joueur *j;
+  
+  // Mutex liste
+  pthread_mutex_lock(&(l->mutex));
+  j = l->j;
+  sprintf(res, "%d", nbTour);
+  while(j != NULL) {
+    sprintf(tmp, "(%s,%d)", j->pseudo, j->score);
+    strcat(res, tmp);
+    j = j->next;
+  }
+  pthread_mutex_unlock(&(l->mutex)); // Lacher mutex
+  return strdup(res);
+}
